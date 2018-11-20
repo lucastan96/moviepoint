c591d08bb48fc1b5f70bbff665fb88b414a9e768
Lucas Tan <lucastan96@gmail.com>
2018-11-20 11:43:55 +0000

    Added fragments, bottom navigation bar with icons

--- app/src/main/java/com/fuego/moviepoint/MainActivity.java
+++ app/src/main/java/com/fuego/moviepoint/MainActivity.java
@@ -1,13 +1,46 @@
 package com.fuego.moviepoint;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.nav_bottom);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new TheaterFragment());
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.nav_bottom_1:
+                fragment = new TheaterFragment();
+                break;
+            case R.id.nav_bottom_2:
+                fragment = new WatchlistFragment();
+                break;
+            case R.id.nav_bottom_3:
+                fragment = new HistoryFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
