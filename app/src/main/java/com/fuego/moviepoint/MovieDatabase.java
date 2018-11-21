10cf4d42e228557c95341dfdb183fa5effd6fc96
Sean Joyce <seanjoyce@outlook.ie>
2018-11-21 15:03:35 +0000

    Added repository

--- app/src/main/java/com/fuego/moviepoint/MovieDatabase.java
+++ app/src/main/java/com/fuego/moviepoint/MovieDatabase.java
@@ -1,10 +1,13 @@
 package com.fuego.moviepoint;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = Movie.class, version = 1)
 public abstract class MovieDatabase extends RoomDatabase {
@@ -18,8 +21,34 @@ public abstract class MovieDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     MovieDatabase.class, "movie_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private MovieDao movieDao;
+
+        private PopulateDbAsyncTask(MovieDatabase db) {
+            movieDao = db.movieDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            movieDao.insert(new Movie("Harry Potter 1", "123.png"));
+            movieDao.insert(new Movie("Harry Potter 2", "542.png"));
+            movieDao.insert(new Movie("Harry Potter 3", "653.png"));
+            return null;
+        }
     }
 }
