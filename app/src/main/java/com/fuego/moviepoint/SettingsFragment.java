8b131a94385b2509971aa85b31855bedea4fca54
Lucas Tan <lucastan96@gmail.com>
2018-11-22 00:07:43 +0000

    Added preference and drawable

---
+++ app/src/main/java/com/fuego/moviepoint/SettingsFragment.java
@@ -0,0 +1,12 @@
+package com.fuego.moviepoint;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+}
